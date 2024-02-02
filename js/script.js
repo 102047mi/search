fa766834ff3bcd702d1c9496ee145d8f721e16b4
Likoy <122590358+102047mi@users.noreply.github.com>
2024-02-02 16:39:01 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let searchInput = document.querySelector('#searchInput');
+let items = document.querySelectorAll('.item');
+
+let chunk = 'Груши';
+
+searchInput.addEventListener('keyup', () => {
+    for (let item of items) {
+        let str = item.textContent;
+        if (str.indexOf(searchInput.value) != -1) {
+            console.log(str);
+        }
+    }
+})
